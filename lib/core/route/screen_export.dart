cd6f05766aae499fcde1c39b452dd89a5fbd5a24
“4th-Pyramid” <fpyramid0@gmail.com>
2024-12-18 11:57:13 +0200

    setup

---
+++ lib/core/route/screen_export.dart
@@ -0,0 +1,28 @@
+export '../../features/onbording/views/onbording_screnn.dart';
+export '../../features/auth/presentation/views/login_screen.dart';
+export '../../features/auth/presentation/views/password_recovery_screen.dart';
+export '../../features/auth/presentation/views/signup_screen.dart';
+
+export 'route_constants.dart';
+export '../../features/bookmark/views/bookmark_screen.dart';
+export '../../features/discover/views/discover_screen.dart';
+export '../../features/home/views/home_screen.dart';
+export '../../features/kids/views/kids_screen.dart';
+export '../../features/on_sale/views/on_sale_screen.dart';
+export '../../features/product/views/product_details_screen.dart';
+
+export '../../features/reviews/view/product_reviews_screen.dart';
+export '../../features/search/views/search_screen.dart';
+export '../../features/address/views/addresses_screen.dart';
+export '../../features/checkout/views/cart_screen.dart';
+
+export '../../features/notification/view/enable_notification_screen.dart';
+export '../../features/notification/view/no_notification_screen.dart';
+export '../../features/notification/view/notification_ontions_screen.dart';
+export '../../features/notification/view/notificatios_screen.dart';
+export '../../features/order/views/orders_screen.dart';
+export '../../features/preferences/views/preferences_screen.dart';
+export '../../features/wallet/views/empty_wallet_screen.dart';
+export '../../features/wallet/views/wallet_screen.dart';
+export '../../features/profile/views/profile_screen.dart';
+export '../../features/user_info/views/user_info_screen.dart';
